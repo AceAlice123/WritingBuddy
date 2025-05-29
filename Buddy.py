e08d4744c9d37a1cb568f7ad683bd31bb0a9834f
AceAlice123 <Intermezzobrilliance@gmail.com>
2025-05-29 22:19:34 +0530

    Added MultiPage and Paragraph Support

--- Buddy.py
+++ Buddy.py
@@ -2,37 +2,113 @@ from PIL import Image, ImageDraw, ImageFont
 import textwrap,random
 
 class Handwriting:
-    def __init__(self,savepath="C:/Users/ashup/Downloads",width=2480,height=3508):
-        self.imgsets = Image.new("RGB", (width, height), "white")
-        self.img = ImageDraw.Draw(self.imgsets)
+    def __init__(self,savepath="./Pages",width=2480,height=2408):
+        self.width =width
+        self.height =height
         self.savepath=savepath
-    def write(self,text,fontsize=36,font_path=["LettersFont\Lettersv2-Regular (2).ttf"],offset = 100,wrap_width=150,weights=[]):
+        self.current_imgset=Image.new("RGB", (self.width, self.height), "white")
+        self.current_img=ImageDraw.Draw(self.current_imgset)
+        self.current_page=1
+        self.current_y=100
+        self.set=False
+        self.fontsize=72
+        self.wrap_width=150
+
+    def __write(self,text,fontsize,font_path,offset = 100,numbers_path=['NumberFont\Digits1-Regular.ttf','NumberFont\Myfont-Regular.otf'],page_number=1,Line_index=0,first=True):
         #iterate to fontpath array to create fonts array
-        fonts=[]
-        times=[]
-        
-        for i in font_path:
-            fonts.append (ImageFont.truetype(i, fontsize))
-            times.append(0)
+       
+        # print(text)
+        print(Line_index)
+        if(not first):
+            imgsets = Image.new("RGB", (self.width, self.height), "white")
+            img = ImageDraw.Draw(imgsets)
+        else:
+            imgsets=self.current_imgset
+            img=self.current_img
+            page_number=self.current_page
+            offset=self.current_y
+            first=False
+        print(page_number)
+        fonts=self.__pathtottf(font_path,fontsize)
+        nfonts=self.__pathtottf(numbers_path,fontsize)
         margin = 110
-        text=self.__randomSpaces(text)
-        for line in textwrap.wrap(text,wrap_width):
-            print(len(line))
-            # Random font index 
-            rint=random.randint(0,len(fonts)-1)
-            self.img.text((margin, offset), line, font=fonts[rint], fill="black")
-            offset += fontsize + random.randint(8,10)
-        self.imgsets.save(self.savepath+"/handwritten_note1.jpg")
-        print("✅ Handwritten note created locally: handwritten_note1.jpg")
+    
+        numbers=['1','2','3','4','5','6','7','8','9','0']
+        remaining=['&','-','_','*','@','~']
+        nonexisting=['-','_']
+        for line in textwrap.wrap(text,self.wrap_width):
+            # print(line)
+            x=margin
+            for char in line:
+                rint=random.randint(0,len(fonts)-1)
+                if (char in nonexisting):
+                    img.text((x, offset), " ", font=nfonts[0], fill="black")
+                    x+=nfonts[0].getmask(char).size[0]
+                elif(char in numbers):
+                    img.text((x, offset), char, font=nfonts[0], fill="black")
+                    x+=nfonts[0].getmask(char).size[0]
+                elif(char in remaining):
+                    # print(char)
+                    img.text((x, offset), char, font=nfonts[1], fill="black")
+                    x+=nfonts[1].getmask(char).size[0]
+                else:
+                    img.text((x, offset), char, font=fonts[rint], fill="black")
+                    x+=fonts[rint].getmask(char).size[0]
+            
+                # print(i,char)
+            Line_index+=1
+            offset += fontsize + random.randint(9,10)
+            # New Page
+            if(offset>=self.height-80):
+                
+                self.__write(self.slice_txt(text,Line_index),fontsize,font_path,100,numbers_path,page_number+1,Line_index,False)
+                break
+        if(not self.set):
+            self.current_imgset=imgsets
+            self.current_img=img
+            self.current_page=page_number
+            self.current_y=offset
+            print(f"offset{offset}")
+            self.set=True
+        imgsets.save(f'''{self.savepath}/handwritten_note{page_number}.jpg''')
+        print(f"✅ Handwritten note created locally: handwritten_note{page_number}.jpg")
     def __randomSpaces(self, text):
+        count=0
         new_text=""
         for i in text:
-            if (i==" " and random.randint(1,200)<20):
+            if i==".":
+                new_text+=" "
+                count+=1
+            elif (i==" " and random.randint(1,200)<20):
                 new_text+="   "
+                count+=3
             elif (i==" " and random.randint(1,200)<40):
                 new_text+="  "
+                count+=2
             elif(i==" " and random.randint(1,100)<60):
                 new_text+=" "
+                count+=1
             new_text+=i
         return new_text
+    
+    def __pathtottf(self,patharray,fontsize):
+        ttfarray=[]
+        for i in patharray:
+            ttfarray.append (ImageFont.truetype(i, fontsize))
+        return ttfarray
+    def write(self, text, fontsize=72,fontpath=["LettersFont\Lettersv2-Regular (2).ttf"]):
+        self.set=False
+        self.fontsize=fontsize
+        text=self.__randomSpaces(text)
+        self.__write(text,fontsize,fontpath)
+    def slice_txt(self,text,line_number):
+        new_text=""
+        index=1
+        wrapobject=textwrap.wrap(text,self.wrap_width)[line_number:]
+        for line in wrapobject:
+                new_text+=line
+        return new_text
+
+
+
 
--- test.py
+++ test.py
@@ -1,8 +1,107 @@
-import random
-text="this is a file."
-new_text=""
-for i in text:
-    if (i==" " and random.randint(1,100)<60):
-        new_text+=" "
-    new_text+=i
-print(new_text)
+import random,textwrap
+text="1this is a file."
+# def f1():
+#     a=1
+#     b=3
+#     return a,b 
+# a,b=f1()
+# print(a,b)
+
+# new_text=""
+# for i in text:
+#     if i=='1':
+#         print("Yes")
+
+text12='''_1.6 C PROGRAMMINGLANGUAGE 
+Prior to writing C programs, it would be interesting to find out what really is C 
+language, how it came into existence and where does it stand with respect to 
+other computer languages. We will briefly outline these issues in the following 
+section. 
+1.6.1 History of  C  Programming Language 
+C is a programming language developed at AT&T’s Bell Laboratory of USA 
+in 1972. It was designed and written by Dennis Ritchie. As compared to other 
+programming languages such as Pascal, C allows a precise control of input and 
+output.  
+Now let us see its historical development. The late 1960s were a turbulent era 
+for computer systems research at Bell Telephone Laboratories. By 1960, many 
+programming languages came into existence, almost each for a specific 
+purpose. For example COBOL was being used for Commercial or Business 
+Applications, FORTRAN for Scientific Applications and so on. So, people 
+started thinking why could not there be a one general purpose language. 
+Therefore, an International Committee was set up to develop such a language, 
+which came out with the invention of ALGOL60. But this language never 
+became popular because it was too abstract and too general. To improve this, a 
+new language called Combined Programming Language (CPL) was developed 
+at CambridgeUniversity. But this language was very complex in the sense that 
+it had too many features and it was very difficult to learn. Martin Richards at 
+CambridgeUniversity reduced the features of CPL and developed a new 
+language called Basic Combined Programming Language (BCPL). But 
+unfortunately it turned out to be much less powerful and too specific. Ken 
+Thompson at AT & T’s Bell Labs, developed a language called B at the same 
+time as a further simplification of CPL. But like BCPL this was also too 
+specific. Ritchie inherited the features of B and BCPL and added some features 
+on his own and developed a language called C. C proved to be quite compact 
+and coherent. Ritchie first implemented C on a DEC PDP-11 that used the 
+UNIX Operating System. 
+Programming
+ Fundamentals
+ For many years the de facto standard for C was the version supplied with the 
+UNIX version 5 operating system. The growing popularity of microcomputers 
+led to the creation of large number of C implementations. At the source code 
+level most of these implementations were highly compatible. However, since 
+no standard existed there were discrepancies. To overcome this situation, 
+ANSI established a committee in 1983 that defined an ANSI standard for the C 
+language.  
+1.6.2 Salient features of  C 
+C is a general purpose%, structured programming language. Among the two 
+types of programming languages discussed earlier, C lies in between these two 
+categories. That’s why it is often called a middle level language. It means that 
+it combines the elements of high level languages with the functionality of
+ Design and develop an efficient algorithm to find the list of prime numbers in 
+the range 501 to 2000. What is the complexity of this algorithm? 
+b) Differentiate between Cubic-time and Factorial-time algorithms. Give example 
+of one algorithm each for these two running times. 
+c) Write an algorithm to multiply two square matrices of order n*n. Also explain 
+the time complexity of this algorithm. 
+d) What are asymptotic bounds for analysis of efficiency of algorithms? Why are 
+asymptotic bounds used? What are their shortcomings? Explain the Big O and 
+Big   notation with the help of a diagram. Find the Big O-notation and Θ
+notation for the function: 
+�
+�(𝑛)= 100𝑛4 +1000𝑛3 +100000 
+e) Write and explain the Left to Right binary exponentiation algorithm. 
+Demonstrate the use of this algorithm to compute the value of 329 (Show the 
+steps of computation). Explain the worst-case complexity of this algorithm. 
+f) Write and explain the Bubble sort algorithm. Discuss its best and worst-case 
+time complexity. 
+g) What are the uses of recurrence relations? Solve the following recurrence 
+relations using the Master’s method 
+a. 𝑇(𝑛) = 4𝑇(𝑛
+ 4
+ ) + 𝑛1 
+b. 𝑇(𝑛) = 4𝑇(3𝑛
+ 4
+ ) + 𝑛1 
+a) What is an Optimisation Problem? Explain with the help of an example. When 
+would you use a Greedy Approach to solve optimisation problem? Formulate the 
+Task Scheduling Problem as an optimisation problem and write a greedy algorithm 
+to solve this problem. Also, solve the following fractional Knapsack problem using 
+greedy approach. Show all the steps. 
+Suppose there is a knapsack of capacity 20 Kg and the following 6 items 
+are to packed in it. The weight and profit of the items are as under: 
+(p1, p2,…, p6) = (30,16,18,20,10, 7) 
+(w1, w2,…, w6) = ( 5, 4, 6, 4, 5, 7) 
+Select a subset of the items that maximises the profit while keeping the total 
+weight below or equal to the given capaci'''
+
+wo=textwrap.wrap(text12)
+print(wo[3:])
+
+
+
+       # if i < len(line) - 1:
+                # # Width from start to next character
+                #     substring = line[:i + 2]
+                #     prev_substring = line[:i + 1]
+                # Accurate width difference
+                # x +=(fonts[rint].getbbox(substring))[2] - (fonts[rint].getbbox(prev_substring))[2]
